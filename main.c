90985c044a88cfbd39fd8c9b744635a2be118807
lvd0611 <1402350618@qq.com>
2024-12-03 16:50:45 +0800

    4

--- main.c
+++ main.c
@@ -3,3 +3,26 @@
 #include "stm32f4xx_rcc.h"
 
 void delay(uint32_t time);
+
+int main(void)
+{
+    GPIO_InitTypeDef GPIO_InitStruct;
+
+    // enable clock for GPIOD
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+
+    // configure GPIOD pin 12 as push-pull output
+    GPIO_InitStruct.GPIO_Pin = GPIO_Pin_12;
+    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
+    GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStruct.GPIO_Speed = GPIO_Speed_100MHz;
+    GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
+    GPIO_Init(GPIOD, &GPIO_InitStruct);
+
+    while (1)
+    {
+        // toggle led
+        GPIO_ToggleBits(GPIOD, GPIO_Pin_12);
+        delay(1000000);
+    }
+}
